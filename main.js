800de8e5a8960de897709edc0655fb4a5f711481
CODINGMASTER197 <100692426+CODINGMASTER197@users.noreply.github.com>
2022-11-02 13:05:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    Image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>;
+    });
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IlGB26bK6/model.json', modelLoaded);
+
+function check(){
+    img = document.getElementById('captured image');
+    classifier.classify(img, gotresult);
+    }
+
+    function gotresult(error, results){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(results)
+            document.getElementById("result_face_name").innerHTML = results[0].label;
+            document.getElementById("result_face_accuracy").innerHTML = results[0].confidence.toFixed(3)
+        }
+    }
+
+    function modelLoaded(){
+        console.log('Model has been Loaded!');
+    }
